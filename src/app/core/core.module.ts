027ca705bf40a4c3356805562affbb98564ecb6f
Melare Marina (CI/DAV3.8) <mml8ca@bosch.com>
2020-07-03 09:59:01 -0300

    Atualização

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AgmCoreModule } from '@agm/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -29,7 +30,8 @@ import { FixedNavMenuComponent } from './fixed-nav-menu/fixed-nav-menu.component
     RouterModule,
     CommonModule,
     NgbModule,
-    AgmCoreModule.forRoot({apiKey: 'AIzaSyBK2ThX75vI3oCxUH2BL4AVcwgcdMQ2ZsI'})
+    AgmCoreModule.forRoot({ apiKey: 'AIzaSyBK2ThX75vI3oCxUH2BL4AVcwgcdMQ2ZsI' }),
+    MDBBootstrapModule.forRoot()
   ],
   exports: [
     NavMenuComponent,
--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -11,17 +11,6 @@ export class HomeComponent implements OnInit {
   longitude = -47.128682;
   isScrolled = false;
   isCollapsed = true;
-  cliGenerate = [
-    { Scaffold: 'Component', Usage: 'ng generate component [name]', Alias: 'ng g c [name]' },
-    { Scaffold: 'Directive', Usage: 'ng generate directive [name]', Alias: 'ng g d [name]' },
-    { Scaffold: 'Pipe', Usage: 'ng generate pipe [name]', Alias: 'ng g p [name]' },
-    { Scaffold: 'Service', Usage: 'ng generate service [name]', Alias: 'ng g s [name]' },
-    { Scaffold: 'Module', Usage: 'ng generate module [name]', Alias: 'ng g m [name]' },
-    { Scaffold: 'Guard', Usage: 'ng generate guard [name]', Alias: 'ng g g [name]' },
-    { Scaffold: 'Enum', Usage: 'ng generate enum [name]', Alias: 'ng g e [name]' },
-    { Scaffold: 'Interface', Usage: 'ng generate interface [name]', Alias: 'ng g i [name]' },
-    { Scaffold: 'Class', Usage: 'ng generate class [name]', Alias: 'ng g cl [name]' },
-  ];
 
   @HostListener('window:scroll', ['$event'])
   checkScroll() {
